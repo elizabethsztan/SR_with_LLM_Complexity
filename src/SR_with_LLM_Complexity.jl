aedcbc5f8548a2105d07b87d473a6c4fc674ba60
Liz Tan <elizabethsztan@gmail.com>
2025-11-05 11:12:28 +0000

    added ComplexityOptions

--- src/SR_with_LLM_Complexity.jl
+++ src/SR_with_LLM_Complexity.jl
@@ -8,15 +8,23 @@ using Reexport
 # Import the specific function we want to override
 import SymbolicRegression.ComplexityModule: compute_complexity
 
-# Include our custom complexity module
-include("LLMComplexity.jl")
-using .LLMComplexity: compute_complexity  # Use our overridden version
+# Include options modules first (order matters!)
+include("LLMComplexityOptionsStruct.jl")
+using .LLMComplexityOptionsStructModule
+
+include("LLMComplexityOptions.jl")
+using .LLMComplexityOptionsModule
 
 # Include other modules
 include("Tools.jl")
 using .Tools
 
-# Export additional functionality if needed
+# Include our custom complexity module
+include("LLMComplexity.jl")
+using .LLMComplexity: compute_complexity  # Use our overridden version
+
+# Export additional functionality
 export string_tree_llm
+export LLMComplexityOptions, ComplexityOptions
 
 end # module SR_with_LLM_Complexity
