98e025a2f9e56f1e49ddcbcdbc8e1ce8637127ab
Liz Tan <elizabethsztan@gmail.com>
2025-12-05 14:10:39 +0000

    removed

--- scripts/sync_hpc_to_laptop.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-rsync -avz --progress \
-  -e "ssh -J eszt2@ssh.maths.cam.ac.uk" \
-  eszt2@swirles.maths.cam.ac.uk:/cephfs/home/eszt2/LLM_Complexity/ \
-  /Users/liz/PhD/LLM_Complexity/SR_with_LLM_Complexity/
-
-echo "Successfully synced Swirles -> Laptop"
